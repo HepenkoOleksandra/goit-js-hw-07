6aec76354af42d97cf3be4dec724f3511cdc9193
HepenkoOleksandra <gepenkoalex@gmail.com>
2024-01-07 00:36:57 +0200

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const email = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+    
+    const userData = {
+        email,
+        password,
+    }
+
+    form.reset();
+
+}
+
+console.log(form);
